129a72008783260242ea763e6833f727bb85b81a
savioramon <savioramon12@gmail.com>
2022-01-02 18:44:55 -0300

    criando componente de formulário

---
+++ src/componentes/SearchFormulario/index.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./style.css";
+
+import { MdClear } from "react-icons/md";
+
+function SearchFormulario() {
+
+    const [textoBusca, setTextoBusca] = useState("");
+
+    function limpaPesquisa() {
+        setTextoBusca("");
+    }
+
+    return (
+        <form 
+            className="formulario-busca" 
+            action="search"
+            method="GET"
+        >
+            <label className="label-pesquisa" htmlFor="texto-pesquisa">
+                <input 
+                    id="texto-pesquisa"
+                    type="text" 
+                    placeholder="Faça uma busca" 
+                    value={textoBusca}
+                    onChange={e => setTextoBusca(e.target.value)}
+                />
+
+                {textoBusca &&
+                    <div className="limpa-busca" onClick={()=>{
+                        setTextoBusca("");
+                    }}>
+                        <MdClear />
+                    </div>
+                }
+
+            </label>
+            
+            <input type="button" value="Buscar" />
+        </form>
+    )
+};
+
+export default SearchFormulario;
