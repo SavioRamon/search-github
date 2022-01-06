51b33ecd31b2752902020d41b024676065eef3dd
savioramon <savioramon12@gmail.com>
2022-01-06 00:41:50 -0300

    criando função para gerar um array de páginas e organizando funções

--- src/componentes/PaginasRepositorios/index.js
+++ src/componentes/PaginasRepositorios/index.js
@@ -1,41 +1,86 @@
-import { useState, useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import "./style.css";
 
 import { useSelector } from "react-redux";
 
-function PaginasRepositorios() {
+import { useParams, useNavigate } from "react-router-dom";
 
-    const [paginasListagem, setPaginasListagem] = useState(0);
+
+function carregaTotalDePaginas(dadosRepositorios) {
+    
+    const todosOsResultados = dadosRepositorios.total_count;
+    const repositoriosPorPagina = dadosRepositorios.items.length;
+    let totalDePaginas;
+    /*
+    totalDePaginas é resultado da divisão entre todos os resultados de uma busca
+    e a quantidade de repositórios exibidos por página.
+        ex: 100 resultados / 20 repositórios por pagina = 5 páginas.
+    */
+    
+    if(todosOsResultados > 1000) {
+        // 1000 é o resultado máximo que a api do github fornece por busca.
+        totalDePaginas = Math.ceil(1000 / repositoriosPorPagina);
+    } else {
+
+        totalDePaginas = Math.ceil(todosOsResultados / repositoriosPorPagina);
+    };
+    
+    return totalDePaginas;
+};
+
+
+function retornaPaginacao(paginasTot, paginaAtual) {
+    // Retorna Array de paginação
+    /*
+        Exemplo de retorno quando a páginaAtual é 5:
+        listaDePaginas = [(1), (...), (3), (4), ((5)), (6), (7), (...), (99)]
+    */
+    
+    let listaDePaginas = [];
+    const primeiraPagina = 1;
+    const ultimaPagina = paginasTot;
+
+    for(let numero = 1; numero <= ultimaPagina; numero++){
+        let valorParaAdicionar;
+        const espacamentoEntrePaginas = Math.abs(paginaAtual - numero);
+
+        (function personalizandoArray(){
+            (numero === primeiraPagina) && (valorParaAdicionar = primeiraPagina);
+        
+            (espacamentoEntrePaginas <= 2) && (valorParaAdicionar = numero);
+    
+            (espacamentoEntrePaginas === 3) && (valorParaAdicionar = "...");   
+            
+            (numero === ultimaPagina) && (valorParaAdicionar = ultimaPagina);
+                
+            valorParaAdicionar && listaDePaginas.push(valorParaAdicionar);
+        }()) 
+    }
+
+    return listaDePaginas;
+}
+
+
+
+export default function PaginasRepositorios() {
+
+    const [arrayPaginas, setArrayPaginas] = useState([]);
 
     const dadosRepositorios = useSelector(state=>state.searchData.repositorios);
 
-
-    function carregaTotalDePaginas() {
-        const todosOsResultados = dadosRepositorios.total_count;
-        const repositoriosPorPagina = dadosRepositorios.items.length;
-        let totalDePaginas;
-        /*
-        totalDePaginas é resultado da divisão entre todos os resultados de uma busca
-        e a quantidade de repositórios exibidos por página.
-            ex: 100 resultados / 20 repositórios por pagina = 5 páginas.
-        */
-        
-        if(todosOsResultados > 1000) {
-            // 1000 é o resultado máximo que a api do github fornece por busca.
-            totalDePaginas = Math.ceil(1000 / repositoriosPorPagina);
-        } else {
-
-            totalDePaginas = Math.ceil(todosOsResultados / repositoriosPorPagina);
-        };
-        
-        return totalDePaginas;
-    };
+    const { pagina=1 } = useParams();
+    const navigate = useNavigate();
 
 
     useLayoutEffect(()=>{
         if(dadosRepositorios && dadosRepositorios.total_count) {
             // Inicia o carregamento das páginas
-            const paginas = carregaTotalDePaginas();
+            const totalDePaginas = carregaTotalDePaginas(dadosRepositorios);
+
+            const paginasEmLista = retornaPaginacao(totalDePaginas, pagina);
+
+            setArrayPaginas(paginasEmLista);
+
         };
     }, [dadosRepositorios]);
 
@@ -49,5 +94,3 @@ function PaginasRepositorios() {
         
     );
 };
-
-export default PaginasRepositorios;
