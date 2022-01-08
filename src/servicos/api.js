e0e8c9d70a46d3f87d3cb752785961408967d9b2
savioramon <savioramon12@gmail.com>
2022-01-08 14:03:21 -0300

    adicionando requisição para único repositório e função de fetch genérica

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -1,13 +1,27 @@
-const BASE_API = "https://api.github.com";
+const BASE_API = "https://api.github.com/search";
 const porPagina = 20;
 
+const basicFetch = async(url)=>{
+    // Função de fetch genérica
+    const req = await fetch(url).then(dados=>dados.json());
+    return req;
+};
+
 export const apiRequisicoes = {
+
     async getRepositorios(textoPesquisa, pagina=0){
         const url =
-        `${BASE_API}/search/repositories?q=${textoPesquisa}/&page=${pagina}&per_page=${porPagina}`;
+        `${BASE_API}/repositories?q=${textoPesquisa}/&page=${pagina}&per_page=${porPagina}`;
 
-        const data = await fetch(url).then(dados=>dados.json());
-        
+        const data = basicFetch(url);
+        return data;
+    },
+
+    async getRepositorioUnico(textoPesquisa){
+        const url = `${BASE_API}/repositories?q=${textoPesquisa}`;
+
+        const data = basicFetch(url);
+
         return data;
     }
-}
+};
