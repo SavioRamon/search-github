bbabbee7a575a18765dd671bdd447ff962a34b08
savioramon <savioramon12@gmail.com>
2022-01-11 20:46:24 -0300

    adicionando as informações do repositório

--- src/telas/Repositorio/index.js
+++ src/telas/Repositorio/index.js
@@ -1,16 +1,155 @@
-
+import React, { useLayoutEffect } from "react";
 import "./style.css";
 
+import { VscRepoForked } from "react-icons/vsc";
+import { BsStarFill, BsEyeFill, BsCodeSlash } from "react-icons/bs";
+import { FaBalanceScale, FaRegCommentAlt } from "react-icons/fa";
+
+import { Creators as searchCreators } from "../../store/ducks/searchData";
+import { useDispatch, useSelector } from "react-redux";
+
 import { useParams } from "react-router-dom";
+
+import moment from "moment";
 
 
 export default function Repositorio() {
 
     const { nomeRepositorio } = useParams();
+    const { dataRepo, dataIssues } = useSelector(state=>state.searchData.repositorioDetalhado);
+    const dispatch = useDispatch();
 
+    useLayoutEffect(()=>{
+        dispatch(searchCreators.getRepositorioDetalhado(nomeRepositorio));
+    }, []);
+
+    function convertendoData(dataISO) {
+        return `Updated ${moment(dataISO).fromNow()}`;
+    }
+    
     return (
-        <div className="repositorio-detalhado">
-            { nomeRepositorio }
-        </div>
-    )
-}
+        <section className="repositorio-detalhado">
+            {dataRepo &&
+                <React.Fragment>
+                    <article className="info-basica-perfil">
+                        <img 
+                            className="imagem-usuario-repositorio" 
+                            src={dataRepo.owner.avatar_url} 
+                            rel="imagem de perfil"
+                        />
+                        <p className="nome-usuario">
+                            {dataRepo.owner.login}
+                        </p>
+                    </article>
+
+
+                    <div className="area-repositorio">
+                        <div className="area-esquerda">
+                            <h1 className="nome-detalhe">{dataRepo.name}</h1>
+                            <p className="descricao-detalhe">{dataRepo.description}</p>
+
+                            <ul className="lista-tags-detalhe">
+                                {dataRepo.topics.map((topico, key)=>{
+                                    return (
+                                        <li key={key}>
+                                            <a target="_blank" href={`https://github.com/topics/${topico}`}>
+                                                {topico}
+                                            </a>
+                                        </li>
+                                    )
+                                })}
+                                
+                            </ul>
+                        </div>
+
+                        <div className="area-direita">
+
+                            <div className="direita-item data">
+                                {convertendoData(dataRepo.update_at)}
+                            </div>
+
+                            <div className="direita-item estrelas">
+                                <span className="star">
+                                    <BsStarFill />
+                                </span>
+                                {dataRepo.stargazers_count}
+
+                            </div>
+
+                            <div className="direita-item viewers">
+                                <span className="eye">
+                                    <BsEyeFill />
+                                </span>
+                                {dataRepo.watchers_count}
+
+                            </div>
+                            
+                            <div className="direita-item forks">
+                                <span className="fork">
+                                    <VscRepoForked />
+                                </span>
+                                {dataRepo.forks_count}
+                                
+                            </div>
+
+                            {dataRepo.language &&
+                                <div className="direita-item linguagem">
+                                    <span className="language">
+                                        <BsCodeSlash />
+                                    </span>
+                                    {dataRepo.language}
+                                </div>
+                            }
+
+                            {dataRepo.license &&
+                                <div className="direita-item licenca">
+                                    <span className="license">
+                                        <FaBalanceScale />
+                                    </span>
+                                    licença {dataRepo.license.spdx_id}
+
+                                </div>
+                            }
+
+                        </div>
+                        
+                    </div>
+
+                    {dataIssues &&
+                        <div className="area-issues">
+                            <h2>Issues</h2>
+                            
+                            <div className="lista-issues">
+                                {dataIssues.map((itemIssue, key)=>{
+                                    if(itemIssue.state === "open") {
+                                        return (
+                                            <div className="item-issue" key={key}>
+                                                <span>{itemIssue.title}</span>
+                                                <div className="comentarios">
+                                                    <span>
+                                                        <FaRegCommentAlt />
+                                                    </span>
+                                                    <span>
+                                                        {itemIssue.comments}
+                                                    </span>
+                                                        
+                                                </div>
+                                            </div>
+    
+                                        )
+                                    }
+                                })
+                                    
+                                }
+                            </div>
+                        </div>
+                    }
+                    
+
+                </React.Fragment>
+                
+                
+            }
+        </section>
+    );
+};
